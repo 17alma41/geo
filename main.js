387e24aaac7b6494d309ea7f24c7991af30f789f
Alvaro <17alma41@gmail.com>
2024-01-18 13:00:27 +0100

    update

--- main.js
+++ main.js
@@ -5,20 +5,20 @@ const opciones = {
     maximumAge: 0
 };
 
-function crearMapa(latitud, longitud){
-    
+function crearMapa(latitude, longitude){
+
     //leafletjs
-    var map = L.map('map').setView([latitud, longitud], 13);
+    var map = L.map('map').setView([latitude, longitude], 13);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker([latitud, longitud]).addTo(map)
+    L.marker([latitude, longitude]).addTo(map)
         .bindPopup('Estas aquí')
         .openPopup();
 
-    var circle = L.circle([latitud, longitud], {
+    var circle = L.circle([latitude, longitude], {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.5,
@@ -28,16 +28,13 @@ function crearMapa(latitud, longitud){
 
 function exito(posicion) {
     console.log(posicion);
-    const latitud = posicion.coords.latitude
-    // console.log(latitud)
-    const longitud = posicion.coords.longitude
-    // console.log(longitud)
-    const accuracy = posicion.coords.accuracy
+
+    const {latitude, longitude, accuracy} = posicion.coords
 
     const p = document.createElement("p");
-    p.innerHTML = `Latitud: ${latitud} <br> Longitud: ${longitud} <br> Precisión: ${accuracy}` 
+    p.innerHTML = `Latitud: ${latitude} <br> Longitud: ${longitude} <br> Precisión: ${accuracy}` 
     document.body.appendChild(p);
-    crearMapa(latitud, longitud)
+    crearMapa(latitude, longitude)
 }
     
 function error(error) {
